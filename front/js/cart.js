77db51d29c1e864a3bbdac74525860f0ee252072
Dylanm13 <valesmoss@hotmail.fr>
2022-02-11 14:31:01 +0100

    ajout de cart.js avec les premiers éléments du produit

---
+++ front/js/cart.js
@@ -0,0 +1,92 @@
+let produitLocalStorage = JSON.parse(localStorage.getItem("product"));
+console.log(produitLocalStorage);
+const positionEmptyCart = document.querySelector("#cart__items");
+
+// Si le panier est vide
+function getCart(){
+if (produitLocalStorage === null || produitLocalStorage == 0) {
+    const emptyCart = `<p>Votre panier est vide</p>`;
+    positionEmptyCart.innerHTML = emptyCart;
+} else {
+for (let product in produitLocalStorage){
+    // Insertion de l'élément "article"
+    let productArticle = document.createElement("article");
+    document.querySelector("#cart__items").appendChild(productArticle);
+    productArticle.className = "cart__item";
+    productArticle.setAttribute('data-id', produitLocalStorage[product].productId);
+
+    // Insertion de l'élément "div"
+    let productDivImg = document.createElement("div");
+    productArticle.appendChild(productDivImg);
+    productDivImg.className = "cart__item__img";
+
+    // Insertion de l'image
+    let productImg = document.createElement("img");
+    productDivImg.appendChild(productImg);
+    productImg.src = produitLocalStorage[product].productImage;
+    productImg.alt = produitLocalStorage[product].productAltTxt;
+    
+    // Insertion de l'élément "div"
+    let productItemContent = document.createElement("div");
+    productArticle.appendChild(productItemContent);
+    productItemContent.className = "cart__item__content";
+
+    // Insertion de l'élément "div"
+    let productItemContentTitlePrice = document.createElement("div");
+    productItemContent.appendChild(productItemContentTitlePrice);
+    productItemContentTitlePrice.className = "cart__item__content__titlePrice";
+    
+    // Insertion du titre h3
+    let productTitle = document.createElement("h2");
+    productItemContentTitlePrice.appendChild(productTitle);
+    productTitle.innerHTML = produitLocalStorage[product].productName;
+
+    // Insertion de la couleur
+    let productColors = document.createElement("p");
+    productTitle.appendChild(productColors);
+    productColors.innerHTML = produitLocalStorage[product].productColor;
+    productColors.style.fontSize = "20px";
+
+    // Insertion du prix
+    let productPrices = document.createElement("p");
+    productItemContentTitlePrice.appendChild(productPrices);
+    productPrices.innerHTML = produitLocalStorage[product].prodcutPrice + " €";
+
+    // Insertion de l'élément "div"
+    let productItemContentSettings = document.createElement("div");
+    productItemContent.appendChild(productItemContentSettings);
+    productItemContentSettings.className = "cart__item__content__settings";
+
+    // Insertion de l'élément "div"
+    let productItemContentSettingsQuantity = document.createElement("div");
+    productItemContentSettings.appendChild(productItemContentSettingsQuantity);
+    productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
+    
+    // Insertion de "Qté : "
+    let productQte = document.createElement("p");
+    productItemContentSettingsQuantity.appendChild(productQte);
+    productQte.innerHTML = "Qté : ";
+
+    // Insertion de la quantité
+    let productQuantity = document.createElement("input");
+    productItemContentSettingsQuantity.appendChild(productQuantity);
+    productQuantity.value = produitLocalStorage[product].quantiteProduit;
+    productQuantity.className = "itemQuantity";
+    productQuantity.setAttribute("type", "number");
+    productQuantity.setAttribute("min", "1");
+    productQuantity.setAttribute("max", "100");
+    productQuantity.setAttribute("name", "itemQuantity");
+
+    // Insertion de l'élément "div"
+    let productItemContentSettingsDelete = document.createElement("div");
+    productItemContentSettings.appendChild(productItemContentSettingsDelete);
+    productItemContentSettingsDelete.className = "cart__item__content__settings__delete";
+
+    // Insertion de "p" supprimer
+    let productSupprimer = document.createElement("p");
+    productItemContentSettingsDelete.appendChild(productSupprimer);
+    productSupprimer.className = "deleteItem";
+    productSupprimer.innerHTML = "Supprimer";
+}
+}}
+getCart();
