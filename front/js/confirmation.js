8eedaba264d50242ef793f336272575bfcb6bc52
Dylanm13 <valesmoss@hotmail.fr>
2022-02-25 12:08:33 +0100

    Ajout du numero de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,6 +1,6 @@
 function main(){
-    const idNode = document.getElementById("orderId")
-    idNode.innerText = localStorage.getItem("orderId")
+    const orderNumber = document.getElementById("orderId")
+    orderNumber.innerText = localStorage.getItem("orderId")
     console.log(localStorage.getItem("orderId"))
     localStorage.clear()
 }
